cbf70cd858fd0e3c9c79e88dde8cbcc58ac41423
eriker75 <eribertmarquez75@gmail.com>
2024-11-02 16:26:31 -0400

    initial structure

---
+++ backend/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { InstitutionsModule } from './institutions/institutions.module';
+import { ArticlesModule } from './articles/articles.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, InstitutionsModule, ArticlesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
